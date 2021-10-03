a8d85bc2ba921e9e401cfd2a8eeed00ac677a14f
uj-git <umangjaiswal20@gmail.com>
2021-10-04 01:00:19 +0530

    added conditionalStatements

---
+++ conditionalStatements/IfElseCondition.java
@@ -0,0 +1,36 @@
+package conditionalStatements;
+
+public class IfElseCondition {
+
+	public static void main(String[] args) {
+//		int number = 50;
+//		
+//		if(number <= 10) {
+//			System.out.println("Number lies between 0-10");
+//		}else if(number > 10 && number <=20) {
+//			System.out.println("Number lies between 11-20");
+//	
+//		}else {
+//			System.out.println("Number is greater than 20");
+//		}
+		
+//		int a = 8;
+//		int b = 15;
+//		
+//		int maxOfTwoNumbers = 0;
+//		
+//		maxOfTwoNumbers = a > b ? a : b ; 
+//		
+//		System.out.println("Max of two numbers is " + maxOfTwoNumbers);
+		
+		int grade = 11;
+		
+		if(grade != 10 || grade != 12) {
+			System.out.println("Not Eligible for boards");
+		}else {
+			System.out.println("Eligible for boards");
+		}
+
+	}
+
+}
