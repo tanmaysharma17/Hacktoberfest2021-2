6544cdfb59c2f945df31e803c0fc93219b4d5a11
czapata <czapata@aconcaguasf.com>
2021-10-10 15:35:48 -0300

    Add JS for statement

---
+++ loops/for-statement.js
@@ -0,0 +1,14 @@
+// Syntax
+// for ([initialization]; [condition]; [final-expression])
+//    statement
+
+// Example
+
+let str = '';
+
+for (let i = 0; i < 9; i++) {
+   str = str + i;
+}
+
+console.log(str);
+// expected output: "012345678"
