ae0cb58c1a80d2f75e947c07c3a479c3c4c073a9
tanmaysharma17 <73175318+tanmaysharma17@users.noreply.github.com>
2021-10-18 20:53:02 +0530

    Update StringsIntroduction.java

--- strings/StringsIntroduction.java
+++ strings/StringsIntroduction.java
@@ -3,7 +3,7 @@ package strings;
 public class StringsIntroduction {
 
 	public static void main(String[] args) {
-		String name = "gulambi";
+		String name = "guljkdfjfjfambi";
 		
 		System.out.println(name.charAt(3));
 
